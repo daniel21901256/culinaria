fbf34d88e784631b6dcbaff06292a2038f0b6a7d
DanielR <a21901256@alunos.ulht.pt>
2021-06-08 08:44:28 +0100

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,10 +81,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "default": env.dj_db_url("DATABASE_URL")
-    }
-}
+      "default": env.dj_db_url("DATABASE_URL")
+ }
 
 
 # Password validation
